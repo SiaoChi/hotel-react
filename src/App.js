ab5f0c90721942a55af08a79977d0244b3ad9a66
siaochi <chichigou.work@gmail.com>
2024-02-28 21:34:03 +0800

    add:rooms page

--- src/App.js
+++ src/App.js
@@ -2,26 +2,27 @@ import './App.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
-import Room from './pages/Room'
-import NoPage from './pages/NoPage';
+import {Home, QA, Contact ,Rooms } from'./pages/Home'
+import  { PageRoom1,PageRoom2,PageRoom3,PageRoom4,PageRoom5,PageRoom6 } from './pages/Room'
 import MenuBar from './components/MenuBar';
 import Footer from './components/Footer';
 
 export default function App() {
-  const scrollToIdSection = ({ id }) => {
-    const aboutSection = document.getElementById(id);
-    if (aboutSection) {
-      aboutSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MenuBar scrollToIdSection={scrollToIdSection} />}>
+        <Route path="/" element={<MenuBar />}>
           <Route index element={<Home />} />
-          <Route path="/rooms1" element={<Room />} />
+          <Route path="/rooms/1" element={<PageRoom1 />} />
+          <Route path="/rooms/2" element={<PageRoom2 />} />
+          <Route path="/rooms/3" element={<PageRoom3 />} />
+          <Route path="/rooms/4" element={<PageRoom4 />} />
+          <Route path="/rooms/5" element={<PageRoom5 />} />
+          <Route path="/rooms/6" element={<PageRoom6 />} />
+          <Route path="/rooms" element={<Rooms />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/qa" element={<QA />} />
         </Route>
       </Routes>
        <Footer />
