58983336988b52f998624f4db11f0e4678f48f6e
siaochi <chichigou.work@gmail.com>
2024-02-29 17:12:48 +0800

    add: mobile menu bar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,7 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
+import { useState } from "react";
 
 
 const MenuBar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const displayMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+  
 
   return (
     <>
@@ -10,6 +17,10 @@ const MenuBar = () => {
         <div className="text-black font-bold text-xl">
           <a href='/'><img className="w-36 md:w-48" src="/yongjen-logo.png" alt="Yong-Jen Logo"></img></a>
         </div>
+
+      
+
+
         <div className="hidden ml-16 md:flex mr-auto">
             <Link to="/" className="text-gray-700 mr-10 hover:text-brand_blue">首頁</Link>
             <Link to="/rooms" className="text-gray-700 mr-10 hover:text-brand_blue">房型</Link>
@@ -25,6 +36,25 @@ const MenuBar = () => {
               <img className="w-6 md:w-8" src="/line.png" alt="Line Icon"></img>
             </a>
         </div>
+
+        {/* 手機版本的菜單 */}
+        <div className="md:hidden">
+          <button onClick={displayMobileMenu} className="ml-auto text-gray-700 hover:text-brand_blue focus:outline-none">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
+
+          {/* 手機版本的選項 */}
+          {isMobileMenuOpen && (
+            <div className="w-1/3 h-screen absolute top-0 right-0 mt-14 bg-white  p-4  shadow-lg">
+              <Link to="/" className="p-2 block text-gray-700 hover:text-brand_blue mb-2">首頁</Link>
+              <Link to="/rooms" className="p-2 block text-gray-700 hover:text-brand_blue mb-2">房型</Link>
+              <Link to="/qa" className="p-2 block text-gray-700 hover:text-brand_blue mb-2">問與答</Link>
+              <Link to="/contact" className="p-2 block text-gray-700 hover:text-brand_blue">聯絡我們</Link>
+            </div>
+          )}
+      </div>
       </div>
     </div>
     <Outlet />
@@ -32,4 +62,5 @@ const MenuBar = () => {
   );
 };
 
-export default MenuBar
+export default MenuBar;
+
