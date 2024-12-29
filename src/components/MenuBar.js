2c0fdb1225753f39d23029e8b42f9b7ec62bfe08
siaochi <chichigou.work@gmail.com>
2024-12-29 23:07:03 +0800

    2024-12-29 update photos

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,14 +1,20 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 
 const MenuBar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
   const displayMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
-  
+
+  // 這裡可以先關閉選單，然後導航到相應的頁面
+  const handleLinkClick = (path) => {
+    displayMobileMenu(false);
+    navigate(path);
+  };
 
   return (
     <>
@@ -26,7 +32,7 @@ const MenuBar = () => {
         </div>
 
         <div className="flex items-center ml-auto">
-            <a href="https://www.facebook.com/p/%E8%A9%A0%E7%8F%8D%E6%96%87%E6%97%85-100086760594011/?paipv=0&eav=AfaDBxYNStjHwju8lxtFlW_VmGuhisr35wiUregb43xhrw9ScpfbDBPoALmhk4ULcI0&_rdr" target="_blank"  rel="noopener noreferrer" className="text-black hover:text-brand_blue mr-2 fb-btn">
+            <a href="https://www.facebook.com/profile.php?id=100086760594011" target="_blank"  rel="noopener noreferrer" className="text-black hover:text-brand_blue mr-2 fb-btn">
               <img className="w-8 md:w-10" src="/facebook.png" alt="FB Icon"></img>
             </a>
             <a  href="https://lin.ee/BPI4HRC" target="_blank" rel="noopener noreferrer" className="line-btn mr-3 text-black hover:text-brand_blue">
@@ -44,11 +50,19 @@ const MenuBar = () => {
 
           {/* 手機版本的選項 */}
           {isMobileMenuOpen && (
+            
             <div className="w-1/3 h-screen absolute top-0 right-0 mt-14 bg-white  p-4  shadow-lg">
-              <Link to="/" className="p-2 block text-gray-700 hover:text-brand_blue mb-2">首頁</Link>
-              <Link to="/rooms" className="p-2 block text-gray-700 hover:text-brand_blue mb-2">房型</Link>
-              <Link to="/qa" className="p-2 block text-gray-700 hover:text-brand_blue mb-2">問與答</Link>
-              <Link to="/contact" className="p-2 block text-gray-700 hover:text-brand_blue">聯絡我們</Link>
+            {/* 關閉選單的 X 按鈕 */}
+            <button 
+              onClick={displayMobileMenu} 
+              className="absolute top-4 right-4 text-xl text-gray-700"
+            >
+              &times; {/* X 字符 */}
+            </button>
+              <Link to="/" onClick={() => handleLinkClick('/')} className="p-2 block text-gray-700 hover:text-brand_blue mb-2">首頁</Link>
+              <Link to="/rooms" onClick={() => handleLinkClick('/rooms')} className="p-2 block text-gray-700 hover:text-brand_blue mb-2">房型</Link>
+              <Link to="/qa" onClick={() => handleLinkClick('/qa')} className="p-2 block text-gray-700 hover:text-brand_blue mb-2">問與答</Link>
+              <Link to="/contact" onClick={() => handleLinkClick('/contact')} className="p-2 block text-gray-700 hover:text-brand_blue">聯絡我們</Link>
             </div>
           )}
       </div>
